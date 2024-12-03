93778457dfa68cf96f1cc45dba66b5df2906649f
Matt DeBoard <matt.deboard@gmail.com>
2024-12-03 14:31:52 -0600

    Use custom events to decouple event handlers
    We now emit "username set" and "client constructed" events that pass
    those values around. This decouples the `promptForUsername` function
    from the handlers for 'data' and 'end' events.

--- src/index.ts
+++ src/index.ts
@@ -19,6 +19,51 @@ const server = telnetlib.createServer({}, c => {
     c.write('Welcome to the Telnet Chat Server!\n');
     promptForUsername(c);
   });
+
+  // Once we set our username inside `promptForUsername`, we can finish
+  // setting up our handlers
+  c.on('username set', (username: string) => {
+    const client = new Client(
+      DEFAULT_ROOM,
+      new Set([DEFAULT_ROOM]),
+      c,
+      username,
+    );
+    ALL_CLIENTS.set(client.username, client);
+    DEFAULT_ROOM.broadcast(
+      client,
+      `${client.username} has joined ${DEFAULT_ROOM.name}!`,
+      true,
+    );
+    client.notifyActiveRoomChange();
+    client.socket.write('Type /help to see available commands\n');
+    client.socket.emit('client constructed', client);
+  });
+
+  c.on('client constructed', (client: Client) => {
+    c.on('data', data => {
+      const msg = data.toString().trim();
+      if (msg.startsWith('/')) {
+        const parsed = parseCommand(client, msg.slice(1));
+        if (parsed) {
+          handleCommand(client, parsed[0], parsed[1]);
+        }
+      } else {
+        handleMessage(client, msg);
+      }
+    });
+
+    c.on('end', () => {
+      ALL_CLIENTS.delete(client.username);
+      for (const room of client.rooms) {
+        room.broadcast(
+          client,
+          `${client.username} disconnected from the server.`,
+          true,
+        );
+      }
+    });
+  });
 });
 
 process.on('SIGINT', () => {
@@ -30,6 +75,7 @@ process.on('SIGINT', () => {
 
 function promptForUsername(c: Client['socket']) {
   c.write('Please enter your username: ');
+
   c.once('data', data => {
     const poss = data.toString().trim();
 
@@ -37,42 +83,7 @@ function promptForUsername(c: Client['socket']) {
       c.write(`Sorry, but ${poss} is already in use!\n`);
       promptForUsername(c);
     } else {
-      const client = new Client(
-        DEFAULT_ROOM,
-        new Set([DEFAULT_ROOM]),
-        c,
-        poss,
-      );
-      ALL_CLIENTS.set(client.username, client);
-      DEFAULT_ROOM.broadcast(
-        client,
-        `${client.username} has joined ${DEFAULT_ROOM.name}!`,
-        true,
-      );
-      client.notifyActiveRoomChange();
-      client.socket.write('Type /help to see available commands\n');
-      c.on('data', data => {
-        const msg = data.toString().trim();
-        if (msg.startsWith('/')) {
-          const parsed = parseCommand(client, msg.slice(1));
-          if (parsed) {
-            handleCommand(client, parsed[0], parsed[1]);
-          }
-        } else {
-          handleMessage(client, msg);
-        }
-      });
-
-      c.on('end', () => {
-        ALL_CLIENTS.delete(client.username);
-        for (const room of client.rooms) {
-          room.broadcast(
-            client,
-            `${client.username} disconnected from the server.`,
-            true,
-          );
-        }
-      });
+      c.emit('username set', poss);
     }
   });
 }
